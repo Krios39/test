8d17d4ac286e9a3307f2d37d9aed2631c2a90379
Krios39 <ryslan12072000@mail.ru>
2021-01-23 15:10:08 +0700

    feature2 commit3

--- src/feature2.js
+++ src/feature2.js
@@ -5,6 +5,7 @@ function feature2(){
         <div>
             <div>feature2 commit1</div>
             <div>feature2 commit2</div>
+            <div>feature2 commit3</div>
         </div>
     )
 }
